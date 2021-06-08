196bdc11c835ef3150d6bf88643e20d1aa0a5dea
KasparsBelovs <kasparsbelovs@yahoo.com>
2021-06-08 17:17:48 +0300

    Start

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FlightPlannerVS.Attributes;
+
+namespace FlightPlannerVS.Controllers
+{
+    [Route ("admin-api/flights/{id}")]
+    [BasicAuthentication]
+    public class AdminApiController : ApiController
+    {
+        public IEnumerable<string> GetFlights(int id)
+        {
+            return new string[] {"value1", "value2"};
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlightPlannerVS.WebApiApplication" Language="C#" %>
