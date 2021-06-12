04370ae93146e3b4ce30cbcca284385d3fcfbe94
KasparsBelovs <kasparsbelovs@yahoo.com>
2021-06-12 19:56:19 +0300

    put ifs in functions

--- Controllers/AdminApiController.cs
+++ Controllers/AdminApiController.cs
@@ -26,45 +26,22 @@ namespace FlightPlannerVS.Controllers
         [HttpPut]
         public IHttpActionResult PutFlight(FlightRequest newFlight)
         {
-            if (newFlight.To == null ||
-                string.IsNullOrEmpty(newFlight.To?.AirportName) ||
-                string.IsNullOrEmpty(newFlight.To?.City) ||
-                string.IsNullOrEmpty(newFlight.To?.Country) ||
-                newFlight.From == null ||
-                string.IsNullOrEmpty(newFlight.From?.AirportName) ||
-                string.IsNullOrEmpty(newFlight.From?.City) ||
-                string.IsNullOrEmpty(newFlight.From?.Country) ||
-                string.IsNullOrEmpty(newFlight.Carrier) ||
-                string.IsNullOrEmpty(newFlight.ArrivalTime) ||
-                string.IsNullOrEmpty(newFlight.DepartureTime))
+            if(IsFlightsPropNullOrEmpty(newFlight))
             {
                 return BadRequest("Check properties, they can't be null or empty");
             }
 
-            if (DateTime.Compare(
-                DateTime.Parse(newFlight.ArrivalTime),
-                DateTime.Parse(newFlight.DepartureTime)) <= 0)
+            if (IsArrivalTimeLMoreThanDepartureTime(newFlight))
             {
                 return BadRequest("Check Departure and Arrival Time");
             }
             
-            if (newFlight.To.AirportName.ToLower().Trim() == newFlight.From.AirportName.ToLower().Trim())
+            if (IsFromAndToAirportsAreSame(newFlight))
             {
                 return BadRequest("City From and To are the same");
             }
 
-            if (
-                (FlightStorage.AllFlights.Any(x =>
-                    x.From.AirportName == newFlight.From.AirportName &&
-                    x.From.City == newFlight.From.City &&
-                    x.From.Country == newFlight.From.Country &&
-                    x.To.AirportName == newFlight.To.AirportName &&
-                    x.To.City == newFlight.To.City &&
-                    x.To.Country == newFlight.To.Country &&
-                    x.Carrier == newFlight.Carrier &&
-                    x.ArrivalTime == newFlight.ArrivalTime &&
-                    x.DepartureTime == newFlight.DepartureTime
-                )))
+            if (IsFlightAlreadyInList(newFlight))
             {
                 return Conflict();
             }
@@ -90,6 +67,48 @@ namespace FlightPlannerVS.Controllers
             FlightStorage.DeleteFlight(id);
 
             return Ok();
+        }
+
+        public bool IsFlightsPropNullOrEmpty(FlightRequest newFlight)
+        {
+            return newFlight.To == null ||
+                   string.IsNullOrEmpty(newFlight.To?.AirportName) ||
+                   string.IsNullOrEmpty(newFlight.To?.City) ||
+                   string.IsNullOrEmpty(newFlight.To?.Country) ||
+                   newFlight.From == null ||
+                   string.IsNullOrEmpty(newFlight.From?.AirportName) ||
+                   string.IsNullOrEmpty(newFlight.From?.City) ||
+                   string.IsNullOrEmpty(newFlight.From?.Country) ||
+                   string.IsNullOrEmpty(newFlight.Carrier) ||
+                   string.IsNullOrEmpty(newFlight.ArrivalTime) ||
+                   string.IsNullOrEmpty(newFlight.DepartureTime);
+        }
+
+        public bool IsArrivalTimeLMoreThanDepartureTime(FlightRequest newFlight)
+        {
+            return DateTime.Compare(
+                DateTime.Parse(newFlight.ArrivalTime),
+                DateTime.Parse(newFlight.DepartureTime)) <= 0;
+        }
+
+        public bool IsFromAndToAirportsAreSame(FlightRequest newFlight)
+        {
+            return newFlight.To.AirportName.ToLower().Trim() == newFlight.From.AirportName.ToLower().Trim();
+        }
+
+        public bool IsFlightAlreadyInList(FlightRequest newFlight)
+        {
+            return (FlightStorage.AllFlights.Any(x =>
+                x.From.AirportName == newFlight.From.AirportName &&
+                x.From.City == newFlight.From.City &&
+                x.From.Country == newFlight.From.Country &&
+                x.To.AirportName == newFlight.To.AirportName &&
+                x.To.City == newFlight.To.City &&
+                x.To.Country == newFlight.To.Country &&
+                x.Carrier == newFlight.Carrier &&
+                x.ArrivalTime == newFlight.ArrivalTime &&
+                x.DepartureTime == newFlight.DepartureTime
+            ));
         }
     }
 }
