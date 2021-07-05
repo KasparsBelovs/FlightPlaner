baf32c3b3f895f5a1426ce0577897ceb2c5c5088
KasparsBelovs <kasparsbelovs@yahoo.com>
2021-07-05 09:12:06 +0300

    Deleted unnecessary files

--- FlightPlannerVS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FlightPlannerVS.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- FlightPlannerVS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FlightPlannerVS.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
